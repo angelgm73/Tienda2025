cc0cea800aacc5cba24a609168f3130448d69bfa
angelgm73 <angelgm73@educastur.es>
2025-02-12 18:44:38 +0100

    Mas informacion añadida.

--- src/main/java/educasturangelgm73/tienda2025/Recordatorio.java
+++ src/main/java/educasturangelgm73/tienda2025/Recordatorio.java
@@ -5,6 +5,7 @@
  * Recuerda que pedidos es un arraylisto
  * articulo y cliente es hashmap y tendrias que poner .values() para usar el stream
  *  System.out.println("Listado de Pedidos:");
+ * toUpperCase() es un método de la clase String en Java que convierte todos los caracteres de la cadena a mayúsculas.
 
  */
 
